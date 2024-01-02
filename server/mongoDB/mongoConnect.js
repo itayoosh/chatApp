316aa67ce2e73eb229b9e61101604afae9399eb5
itay_dev <135500267+itayoosh@users.noreply.github.com>
2024-01-02 19:58:31 +0200

    Delete server directory

--- server/mongoDB/mongoConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-require("dotenv").config();
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(process.env.MONGO_DB);
-  console.log("mongo connected");
-}
